cf208852ed851cb177cbdac22aa09e1c97caea3c
s-t-h <simon.hackl@uni-tuebingen.de>
2024-03-14 13:30:58 +0100

    - Added descriptions, authors and mail adresses to setup.py.

---
+++ ptmvision/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="ptmvision",
+    packages=["ptmvision"],
+    version="1.0",
+    author="Caroline Jachmann,Simon Hackl",
+    author_email="caroline.jachmann@ugent.be,simon.hackl@uni-tuebingen.de",
+    description="PTMVision is an interactive web platform for effortless exploratory visual analysis of post translational modifications (PTMs).",
+    include_package_data=True,
+    install_requires=[
+        "flask",
+        "Flask-Session",
+        "python-dotenv",
+        "numpy",
+        "datetime",
+        "brotli",
+        "gunicorn",
+        "psm_utils",
+        "biopython",
+        "unimod-mapper",
+        "scipy",
+    ],
+)
