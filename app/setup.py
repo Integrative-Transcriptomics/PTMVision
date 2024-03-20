abc0270f393ccbd72d6788e0cdeeba66642bb4d3
s-t-h <simon.hackl@uni-tuebingen.de>
2024-03-20 13:44:22 +0100

    - Updated version.

--- app/setup.py
+++ app/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="ptmvision",
     packages=["ptmvision"],
-    version="1.0",
+    version="1.1",
     author="Caroline Jachmann,Simon Hackl",
     author_email="caroline.jachmann@ugent.be,simon.hackl@uni-tuebingen.de",
     description="PTMVision is an interactive web platform for effortless exploratory visual analysis of post translational modifications (PTMs).",
