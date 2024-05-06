74b8c7bcb875065a966d515ba46b7f9cbdd7cd70
s-t-h <simon.hackl@uni-tuebingen.de>
2024-05-06 20:09:49 +0200

    - Updated requirments to latest changes with session cache. - Removed unused entries from setup.py.

--- app/setup.py
+++ app/setup.py
@@ -7,18 +7,4 @@ setup(
     author="Caroline Jachmann,Simon Hackl",
     author_email="caroline.jachmann@ugent.be,simon.hackl@uni-tuebingen.de",
     description="PTMVision is an interactive web platform for effortless exploratory visual analysis of post translational modifications (PTMs).",
-    include_package_data=True,
-    install_requires=[
-        "flask",
-        "Flask-Session",
-        "python-dotenv",
-        "numpy",
-        "datetime",
-        "brotli",
-        "gunicorn",
-        "psm_utils",
-        "biopython",
-        "unimod-mapper",
-        "scipy",
-    ],
 )
