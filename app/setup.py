5e33528323f858042f50ce95f7565d9835629d98
s-t-h <simon.hackl@uni-tuebingen.de>
2024-05-10 23:41:02 +0200

    Updated license information.

--- app/setup.py
+++ app/setup.py
@@ -5,6 +5,7 @@ setup(
     packages=["ptmvision"],
     version="1.1",
     author="Caroline Jachmann,Simon Hackl",
+    license="GPL-3.0",
     author_email="caroline.jachmann@ugent.be,simon.hackl@uni-tuebingen.de",
     description="PTMVision is an interactive web platform for effortless exploratory visual analysis of post translational modifications (PTMs).",
 )
